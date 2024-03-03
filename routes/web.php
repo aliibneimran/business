ea1cf143b2964701f0d4dd5d633586f6c91a0b6d
unknown <you@example.com>
2024-03-03 10:59:46 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::prefix('admin')->group(function(){
+    Route::get('login',[AdminController::class,'index'])->name('admin_login_form');
+    Route::post('login/owner',[AdminController::class,'login'])->name('admin_login');
+    Route::get('dashboard',[AdminController::class,'dashboard'])->name('admin_dashboard')->middleware('admin');
+    Route::get('/logout',[AdminController::class,'logout'])->name('admin_logout')->middleware('admin');
+    Route::get('register',[AdminController::class,'register'])->name('admin_register');
+    Route::post('register/create',[AdminController::class,'registration'])->name('admin.register.create');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
